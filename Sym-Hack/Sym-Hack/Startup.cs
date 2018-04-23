14827e79576fbf8491adcc936cffc10a5590447e
erinbradley27 <erinbradley27@hotmail.com>
2018-04-23 11:53:42 -0400

    Created project

---
+++ Sym-Hack/Sym-Hack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sym_Hack.Startup))]
+namespace Sym_Hack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
