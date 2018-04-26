8c04d2934a081e1f263cfc120c7603a520d784a2
erinbradley27 <erinbradley27@hotmail.com>
2018-04-26 10:13:51 -0400

    Fixed database - indexes do not work with migrations

--- Sym-Hack/Sym-Hack.Domain/SymHackContext.cs
+++ Sym-Hack/Sym-Hack.Domain/SymHackContext.cs
@@ -24,25 +24,6 @@ namespace Sym_Hack.Domain
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            #region Fix asp.net identity 2.0 tables under MySQL
-            // Explanations: primary keys can easily get too long for MySQL's 
-            // (InnoDB's) stupid 767 bytes limit.
-            // With the two following lines we rewrite the generation to keep
-            // those columns "short" enough
-            modelBuilder.Entity<IdentityRole>()
-                .Property(c => c.Name)
-                .HasMaxLength(128)
-                .IsRequired();
-
-            // We have to declare the table name here, otherwise IdentityUser 
-            // will be created
-            modelBuilder.Entity<SymHackUser>()
-                .ToTable("AspNetUsers")
-                .Property(c => c.UserName)
-                .HasMaxLength(128)
-                .IsRequired();
-            #endregion
         }
     }
 }
