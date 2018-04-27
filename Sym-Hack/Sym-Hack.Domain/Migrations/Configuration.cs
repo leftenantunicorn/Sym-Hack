09af4b6d8ad5b9077c642d20d4d0aa81dd0b24f6
erinbradley27 <erinbradley27@hotmail.com>
2018-04-27 09:23:11 -0400

    Add migrations to code

--- Sym-Hack/Sym-Hack.Domain/Migrations/Configuration.cs
+++ Sym-Hack/Sym-Hack.Domain/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace Sym_Hack.Domain.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Sym_Hack.Domain.SymHackContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Sym_Hack.Domain.SymHackContext>
     {
         public Configuration()
         {
